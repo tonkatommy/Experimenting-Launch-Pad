b77fac9d075d2b5fba5cbca4290e59fd49f697f0
Thomas Goodman <tommysdevelopment@gmail.com>
2022-06-17 17:26:08 +1200

    Initial Upload

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+  [SerializeField] float mainThrust = 1000f;
+  [SerializeField] float mainRotation = 100f;
+  Rigidbody rocketRigidbody;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rocketRigidbody = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    ProcessTrust();
+    ProcessRotation();
+  }
+
+  void ProcessTrust()
+  {
+    if (Input.GetKey(KeyCode.Space))
+    {
+      rocketRigidbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+    }
+  }
+
+  void ProcessRotation()
+  {
+    if (Input.GetKey(KeyCode.A))
+    {
+      ApplyRotation(mainRotation);
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+      ApplyRotation(-mainRotation);
+    }
+  }
+
+  private void ApplyRotation(float rotationThisFrame)
+  {
+    rocketRigidbody.freezeRotation = true; // freezing rotation to manually rotate
+    transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+    rocketRigidbody.freezeRotation = false; // unfreezing rotation for the physics sytem
+  }
+}
